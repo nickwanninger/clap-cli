77b14a5247cfad223557a49030eb5862f29a7e60
Nick Wanninger <nickwanninger@gmail.com>
2018-01-18 17:12:41 -0600

    stdin support

--- cli.js
+++ cli.js
@@ -2,4 +2,15 @@
 
 const clap = require('./index')
 
-console.log(clap(process.argv.splice(2).join(' ')))
+const words = process.argv.splice(2).join(' ')
+if (words.length > 0) {
+	// print the args but clapped
+	console.log(clap(words))
+} else {
+	// print the data from stdin
+	process.stdin.on('data', function(data) {
+		data = data.toString('utf8')
+		console.log(clap(data))
+	})
+	process.stdin.on('end', () => process.exit(0))
+}
--- readme.md
+++ readme.md
@@ -1,19 +1,27 @@
 # clap-cli
 
-
 It just puts claps between words.
 
+---
 
-----
 Installation
+
 ```bash
 npm install -g clap-cli
 ```
 
-----
+---
+
 The only command:
+
 ```bash
 clap wow thats really cool
 > wow 👏 thats 👏 really 👏 cool
 ```
 
+or you can pipe stdin:
+
+```bash
+echo "wow thats really cool" | clap
+> wow 👏 thats 👏 really 👏 cool
+```
