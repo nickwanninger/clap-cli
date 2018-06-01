344e80621df4d9f424739d43fd325d809e0c1c73
Nick Wanninger <nickwanninger@gmail.com>
2018-05-31 20:53:02 -0400

    Remove tests.
    Signed-off-by: Nick Wanninger <nickwanninger@gmail.com>

--- test/index.js
+++
@@ -1,8 +0,0 @@
-const assert = require('assert')
-const clap = require('../index.js')
-
-describe('clap', () => {
-	it('should insert claps between words', () => {
-		assert.equal(clap('this is a test'), 'this 👏 is 👏 a 👏 test')
-	})
-})
